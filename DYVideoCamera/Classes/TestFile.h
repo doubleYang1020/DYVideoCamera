20606c81ec580865816c5dc4f9af5ea63b5e0375
huyangyang <huyangyang@videopls.com>
2019-08-23 18:30:32 +0800

    x

---
+++ DYVideoCamera/Classes/TestFile.h
@@ -0,0 +1,16 @@
+//
+//  TestFile.h
+//  DYVideoCamera
+//
+//  Created by videopls on 2019/8/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestFile : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DYVideoCamera/Classes/TestFile.m
@@ -0,0 +1,12 @@
+//
+//  TestFile.m
+//  DYVideoCamera
+//
+//  Created by videopls on 2019/8/23.
+//
+
+#import "TestFile.h"
+
+@implementation TestFile
+
+@end
